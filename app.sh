f7762dceb686d3289fc6e2e517d4eaf266a3e2af
Declan Andrew <anysweetimes@naver.com>
2022-04-26 05:54:06 +0900

    refactor: 명령어 자체를 환경변수에 대입하는 것으로 수정

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/codingx/miran/deep-text-recognition-benchmark/ocr-server
 
-result = `. /home/ubuntu/anaconda3/bin/activate pytorch_p36 | python ../demo.py --image_folder ./upload/ --saved_model ../saved_models/TPS-ResNet-BiLSTM-CTC-Seed1111/best_accuracy.pth --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction CTC`
+result = `/home/ubuntu/anaconda3/envs/pytorch_p36/bin/python ../demo.py --image_folder ./upload/ --saved_model ../saved_models/TPS-ResNet-BiLSTM-CTC-Seed1111/best_accuracy.pth --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction CTC`
 
 echo "$result"
