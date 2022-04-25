9baf9be34ab59da34b53f7e0a1bf3d31bf9c3434
Declan Andrew <anysweetimes@naver.com>
2022-04-26 05:33:48 +0900

    refactor: bash 명령어 수정

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/ubuntu/anaconda3/bin/activate pytorch_p36
+. /home/ubuntu/anaconda3/bin/activate pytorch_p36
 cd /home/codingx/miran/deep-text-recognition-benchmark/ocr-server
 result = $(python ../demo.py --image_folder ./upload/ --saved_model ../saved_models/TPS-ResNet-BiLSTM-CTC-Seed1111/best_accuracy.pth \
       --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction CTC)
